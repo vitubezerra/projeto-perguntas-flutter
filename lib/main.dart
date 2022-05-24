f6f39f0e06b435abe2ec2da1ea3f6d9cfe63dffb
Victor <bezerra.victor15@gmail.com>
2022-05-23 23:45:11 -0300

    Aula 67

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : const Resultado(),
+            : Resultado(_pontuacaoTotal),
       ),
     );
   }
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({Key? key}) : super(key: key);
+  final int pontuacao;
+
+  const Resultado(this.pontuacao, {Key? key}) : super(key: key);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Text(
-        'Parabéns!',
-        style: TextStyle(fontSize: 28),
+        fraseResultado,
+        style: const TextStyle(fontSize: 28),
       ),
     );
   }
